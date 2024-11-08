28b4af7efa7726a14d9669ab485d492f58a2de62
Animesh Chakraborty <animesh.aotr@gmail.com>
2024-11-09 00:33:24 +0530

    Add daily logging configuration for Goinfinito and CURL requests

--- src/config/logging.php
+++ src/config/logging.php
@@ -14,5 +14,17 @@ return [
             'level' => 'debug',
             'days' => 7, // logs will be retained for 7 days
         ],
+        'goinfinito' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/third_party/goinfinito/log.log'),
+            'level' => 'debug',
+            'days' => 7,
+        ],
+        'curl' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/external_curl/log.log'),
+            'level' => 'debug',
+            'days' => 7,
+        ],
     ],
 ];
